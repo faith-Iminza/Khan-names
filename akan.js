fa2874e72d951af6422ed78bcb4d63c82288de18
faith <faithjacks63@gmail.com>
2019-07-14 14:00:12 -0400

    add variable and function inputs in js

---
+++ akan.js
@@ -0,0 +1,8 @@
+var cen, year, month, date;
+
+function input(){
+  cen = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  date = parseInt(document.getElementById("date").value);
+}
